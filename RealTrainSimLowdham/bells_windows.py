4808279027565c7127c41684636607c683b7d664
David Moore <david@axitech.co.uk>
2025-09-06 16:39:41 +0100

    Started to add uvicorn

--- RealTrainSimLowdham/bells_windows.py
+++ RealTrainSimLowdham/bells_windows.py
@@ -1,4 +1,4 @@
-import time
+# import time
 import winsound
 
 # from time import sleep
---
+++ build-uv.bat
@@ -0,0 +1,15 @@
+@echo off
+REM Create virtual environment
+uv venv .venv
+
+REM Install dependencies
+uv sync --extra dev
+
+REM Run tests
+uv run pre-commit run --all-files
+rem uv run pytest --cov -vv tests
+rem uv run coverage report --fail-under=85
+
+REM Build
+uv run pyinstaller RealTrainSimLowdham.spec --clean 2> dist\build.log
+tail dist\build.log
