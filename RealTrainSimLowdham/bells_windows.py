50d2c74d3958d098e56a7b759ca6734a7db6c6ad
David Moore <david@axitech.co.uk>
2025-09-06 16:13:13 +0100

    Added 'stop sound' before each sound, rather than a delay after.

--- RealTrainSimLowdham/bells_windows.py
+++ RealTrainSimLowdham/bells_windows.py
@@ -3,22 +3,25 @@ import winsound
 
 # from time import sleep
 
-gap_period = 0.6
+gap_period = 0.5
 
 
 def up_bell():
     # winsound.Beep(2200, 50)
+    winsound.PlaySound(None, 0)
     winsound.PlaySound("/sigbox/sounds/ding2.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
-    time.sleep(gap_period)
+    # time.sleep(gap_period)
 
 
 def down_bell():
     # winsound.Beep(1600, 50)
+    winsound.PlaySound(None, 0)
     winsound.PlaySound("/sigbox/sounds/ding3.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
-    time.sleep(gap_period)
+    # time.sleep(gap_period)
 
 
 def bell_tapper():
     # winsound.Beep(400, 50)
+    winsound.PlaySound(None, 0)
     winsound.PlaySound("/sigbox/sounds/tap.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
-    time.sleep(gap_period)
+    # time.sleep(gap_period)
